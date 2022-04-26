de78236a18f3e1e8d1e47a222e62bdfa745f24d5
Jascha <jascha3600@gmail.com>
2022-04-26 10:38:28 +0200

    refactored code

---
+++ src/routes/user-routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/user-controllers");
+
+router.post("/api/user", controller.addUser);
+
+router.get("/api/user", controller.getAllUsers);
+
+router.get("/api/user/profile", controller.getUserProfile);
+
+router.get("/api/user/:userID", controller.getUserById);
+
+router.put("/api/user/:userID", controller.updateUser);
+
+router.delete("/api/user/:userID", controller.deleteUserById);
+module.exports = router;
