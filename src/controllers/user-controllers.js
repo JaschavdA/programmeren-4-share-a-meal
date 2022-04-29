3a884fb7fdb9a14793d22eeac0ae8cab4af4d63f
Jascha <jascha3600@gmail.com>
2022-04-29 12:56:47 +0200

    Added mySQL database

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -1,5 +1,5 @@
 const req = require("express/lib/request");
-
+const dbconnection = require("../../database/dbconnection");
 // TODO: Start using this to validate input
 const assert = require("assert");
 
@@ -63,10 +63,28 @@ let controller = {
   },
 
   getAllUsers: (req, res) => {
-    res.status(200).json({
-      status: 200,
-      result: database,
-      extra_info: "authorization not yet implemented",
+    dbconnection.getConnection(function (err, connection) {
+      if (err) throw err; // not connected!
+
+      // Use the connection
+      connection.query("SELECT * FROM user", function (error, results, fields) {
+        // When done with the connection, release it.
+        connection.release();
+
+        // Handle error after the release.
+        if (error) throw error;
+
+        console.log("#results = ", results.length);
+        console.log(results);
+        res.status(200).json({
+          status: 200,
+          result: results,
+        });
+
+        // pool.end((err) => {
+        //   console.log("pool was closed");
+        // });
+      });
     });
   },
 
