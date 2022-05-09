2edc1cb07622960297e5ea4cc6bab4fbb43d6a05
Jascha <jascha3600@gmail.com>
2022-05-09 22:56:15 +0200

    added comment

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -95,32 +95,7 @@ let controller = {
     });
   },
 
-  // getAllUsers: (req, res) => {
-  //   dbconnection.getConnection(function (err, connection) {
-  //     if (err) throw err; // not connected!
-
-  //     // Use the connection
-  //     connection.query("SELECT * FROM user", function (error, results, fields) {
-  //       // When done with the connection, release it.
-  //       connection.release();
-
-  //       // Handle error after the release.
-  //       if (error) throw error;
-
-  //       console.log("#results = ", results.length);
-  //       console.log(results);
-  //       res.status(200).json({
-  //         status: 200,
-  //         result: results,
-  //       });
-
-  //       // pool.end((err) => {
-  //       //   console.log("pool was closed");
-  //       // });
-  //     });
-  //   });
-  // },
-
+  //Gets user by id
   getUserById: (req, res) => {
     dbconnection.getConnection(function (err, connection) {
       if (err) throw err; // not connected!
